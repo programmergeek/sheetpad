f64e8b58d2645aa1c32019aaff8a848d07895960
programmergeek <programergeek@gmail.com>
2021-11-14 08:18:44 +0200

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,21 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      sm: "400px",
+      md: "800px",
+      lg: "1200px",
+      xl: "1600px",
+    },
+    colors: {
+      orange: {
+        lightOrange: "#F3BD91",
+        darkOrange: "#F57207",
+      },
+    },
+    fontFamily: {
+      openSans: ["Open Sans", "sans-serif"],
+    },
     extend: {},
   },
   variants: {

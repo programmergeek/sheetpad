b1bcb35d0cc190ea35284a7c6f7bd67495569cdd
programmergeek <programergeek@gmail.com>
2021-11-22 22:45:12 +0200

    create and implement icon component

--- src/Components/Buttons.tsx
+++ src/Components/Buttons.tsx
@@ -1,30 +1,40 @@
 import React from "react";
 
 type IconProps = {
-  image?: string; // string value for image
-  svgImage?: Element; // accepts svg graphic container
+  icon: string; // icon image/svg location
+  className?: string; // class names for icon
   onClick?: () => void; // event listener for if clicked
+};
+
+const Icon: React.FC<{ icon: string; className: string }> = ({ ...props }) => {
+  return (
+    <div>
+      <img
+        src={props.icon}
+        alt="icon"
+        className={`w-10 h-10 ${props.className}`}
+      />
+    </div>
+  );
 };
 
 /**
  * Purpose: Creates circular button component with image or svg graphic in the center
- * @param {string} image - [optional] image location
- * @param {Element} svgImage - [optional] svg container
+ * @param {string} icon - icon image/svg graphic location [string]
  * @param {() => void} onClick - [option] click event listener
  */
 
 export const IconButton: React.FC<IconProps> = ({ ...props }: IconProps) => {
   return (
     <button
-      className="w-10 rounded-full h-10 shadow-icon m-4"
+      className="w-15 rounded-full h-15 shadow-icon m-4 p-2 bg-white"
       onClick={props.onClick}
     >
       {/*if image is provided use that else assume svg graphic has been provided */}
-      {props.image ? (
-        <img src={props.image} alt="icon-button" className="w-9 h-9" />
-      ) : (
-        props.svgImage
-      )}
+      <Icon
+        icon={props.icon}
+        className={`${props.className ? props.className : ""}`}
+      />
     </button>
   );
 };

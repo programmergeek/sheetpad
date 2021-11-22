3328fc0a953b60977306a462d6aed5584d6a6605
programmergeek <programergeek@gmail.com>
2021-11-22 22:45:59 +0200

    implement icon component

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { IconButton, Tag } from ".";
+import edit from "./../assets/svg/Edit Icon.svg";
+import close from "./../assets/svg/Close.svg";
 
 interface Props {
   header: string;
@@ -23,8 +25,8 @@ export const Card: React.FC<Props> = ({ ...props }: Props) => {
       <h3 className="font-semibold text-3xl text-left">{props.header}</h3>
       <p className="text-gray-500 text-left text-sm">@{props.author}</p>
       <p className="text-base text-left">{props.sampleText}</p>
-      <IconButton />
-      <IconButton />
+      <IconButton icon={edit} className="transform rotate-45" />
+      <IconButton icon={close} className="w-9 h-9" />
     </div>
   );
 };
